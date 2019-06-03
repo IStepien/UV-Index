056bf8e1c4b8f5f14f292d0f28a101d9188be70d
Irmina Stepien <irm.stepien@gmail.com>
2019-06-03 09:28:44 +0200

    ApiService getCurrentUVData method added

--- app/src/main/java/com/example/uv_index/ApiService.java
+++ app/src/main/java/com/example/uv_index/ApiService.java
@@ -1,4 +1,9 @@
 package com.example.uv_index;
 
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiService {
+    @GET("data/2.5/uvi?")
+    Call<ApiResponse> getCurrentUVData(@Query("lat") String lat, @Query("lon") String lon, @Query("AppID") String app_id)
 }
