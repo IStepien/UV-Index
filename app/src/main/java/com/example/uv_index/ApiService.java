332920442b765908b854a432019d49f1d63793ae
Irmina Stepien <irm.stepien@gmail.com>
2019-06-04 10:29:53 +0200

    ApiService modified

--- app/src/main/java/com/example/uv_index/ApiService.java
+++ app/src/main/java/com/example/uv_index/ApiService.java
@@ -1,10 +1,20 @@
 package com.example.uv_index;
 
+
+
+import com.example.uv_index.model.ResponseResult;
+
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 
 public interface ApiService {
-    @GET("api/v1/uv")
-    Call<UVDataModel> getCurrentUVData(@Query("lat") String lat, @Query("lon") String lon, @Query("AppID") String app_id);
+
+    String apiKey = BuildConfig.ApiKey;
+
+    @Headers("x-access-token: "+apiKey)
+    @GET("api/v1/uv?")
+    Call<ResponseResult> getCurrentUVData(@Query("lat") String lat, @Query("lng") String lng);
 }
