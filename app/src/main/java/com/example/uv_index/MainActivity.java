10fb359d3cd6c62319150bbbd9da3b0643910857
Irmina Stepien <irm.stepien@gmail.com>
2019-06-05 11:03:42 +0200

    LocationListener added

--- app/src/main/java/com/example/uv_index/MainActivity.java
+++ app/src/main/java/com/example/uv_index/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.uv_index;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText latEditText;
     private EditText longEditText;
     private TextView uvDataTextView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,50 @@ public class MainActivity extends AppCompatActivity {
         latEditText = findViewById(R.id.latitudeEditText);
         longEditText = findViewById(R.id.longitudeEditText);
         uvDataTextView = findViewById(R.id.UVDataTextView);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                Log.i("LOCATION", location.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        else{
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+            }
+        }
     }
 
     public void getUVData(View view) {
@@ -66,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
     private void setBackgroundColor(Double uvValue) {
 
         if (uvValue >= 0 && uvValue < 3) {
-            
+
             uvDataTextView.setBackgroundResource(R.color.colorUvLevelLow);
         }
         if (uvValue >= 3 && uvValue < 6) {
