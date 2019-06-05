718ccd3681f98f0d5ccbd5b2d2d4233ec2811b91
Irmina Stepien <irm.stepien@gmail.com>
2019-06-05 13:49:05 +0200

    Geocoder added and getUVData method modified

--- app/src/main/java/com/example/uv_index/MainActivity.java
+++ app/src/main/java/com/example/uv_index/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -19,7 +21,10 @@ import android.widget.TextView;
 
 import com.example.uv_index.model.ResponseResult;
 
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,19 +32,19 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText latEditText;
-    private EditText longEditText;
+    private TextView currentLocation;
     private TextView uvDataTextView;
     private LocationManager locationManager;
     private LocationListener locationListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        latEditText = findViewById(R.id.latitudeEditText);
-        longEditText = findViewById(R.id.longitudeEditText);
+
+        currentLocation = findViewById(R.id.currentLocation);
         uvDataTextView = findViewById(R.id.UVDataTextView);
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -47,7 +52,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLocationChanged(Location location) {
 
-                Log.i("LOCATION", location.toString());
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listAdresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    if (listAdresses != null && listAdresses.size() > 0) {
+                        currentLocation.setText(listAdresses.get(0).getLocality());
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                getUVData(location);
             }
 
             @Override
@@ -67,30 +82,30 @@ public class MainActivity extends AppCompatActivity {
         };
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-        else{
+        } else {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 
         }
+
 
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 
             }
         }
     }
 
-    public void getUVData(View view) {
+    public void getUVData(Location location) {
 
         ApiService apiService = RetrofitInstance.getService();
-        Call<ResponseResult> call = apiService.getCurrentUVData(latEditText.getText().toString(), longEditText.getText().toString());
+        Call<ResponseResult> call = apiService.getCurrentUVData(String.valueOf(location.getLatitude()), String.valueOf(location.getLongitude()));
 
 
         call.enqueue(new Callback<ResponseResult>() {
